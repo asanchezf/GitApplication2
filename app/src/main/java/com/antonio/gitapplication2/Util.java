4e09fc90fd2685e15b9a0169c38449dbc56d9a92
asanchezf <antoniom.sanchezf@gmail.com>
2019-10-08 19:21:15 +0200

    Cambio en Util desde local

--- app/src/main/java/com/antonio/gitapplication2/Util.java
+++ app/src/main/java/com/antonio/gitapplication2/Util.java
@@ -4,7 +4,12 @@ package com.antonio.gitapplication2;
  * Created by Antonio Sánchez -- 08/10/2019. -- company Ltd -- antoniom.sanchezf@gmail.com
  */
 public class Util {
+    private void desdeLocal(){}
+
+
 
     private void paraAmend(){}
+
+
 
 }

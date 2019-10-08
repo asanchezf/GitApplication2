ddd8c6c9b7474dbdb1c3bfc4bfd45f84d3154100
asanchezf <antoniom.sanchezf@gmail.com>
2019-10-08 12:35:56 +0200

    Se modifica MainActivity introduciendo onStart()

--- app/src/main/java/com/antonio/gitapplication2/MainActivity.java
+++ app/src/main/java/com/antonio/gitapplication2/MainActivity.java
@@ -16,4 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
     }
+
+    protected void onStart(){
+        super.onStart();
+    }
 }

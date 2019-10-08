8018bd16cb8bf4507c4ddbbadb7a0314f1b8893d
asanchezf <antoniom.sanchezf@gmail.com>
2019-10-08 12:50:44 +0200

    Se añade Util.clas

---
+++ app/src/main/java/com/antonio/gitapplication2/Util.java
@@ -0,0 +1,10 @@
+package com.antonio.gitapplication2;
+
+/**
+ * Created by Antonio Sánchez -- 08/10/2019. -- company Ltd -- antoniom.sanchezf@gmail.com
+ */
+public class Util {
+
+    private void paraAmend(){}
+
+}
